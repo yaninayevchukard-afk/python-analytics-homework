e1015b5f056fdd49a4cc3231f48d6e2913e7cf07
Yanina Yevchuk <1000000millionov@gmail.com>
2026-02-27 16:25:59 +0100

    update Lviv sales to 950

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
-data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
+data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 df = pd.DataFrame(data)
 print("Продажі по містах:")
 print(df)
-print("Середнє значення:", df["sales"].mean())
+print("Середне нє значення:", df["sales"].mean())
